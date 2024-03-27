c4692683156e2ef4c090c45228640a96d690cc49
Willys12 <ben.willys92@gmail.com>
2024-03-27 13:48:20 +0300

    Calculating the height of BT

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,16 +6,23 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height;
-size_t right_height;
+size_t left_height = 0;
+size_t right_height = 0;
 
-if (tree == NULL)
+if (tree)
+{
+if ((*tree).left)
+{
+left_height = 1 + binary_tree_height((*tree).left);
+}
+if ((*tree).right)
+{
+right_height = 1 + binary_tree_height((*tree).right);
+}
+return (left_height > right_height) ? left_height : right_height;
+}
+else
 {
 return (0);
 }
-
-left_height = binary_tree_height((*tree).left);
-right_height = binary_tree_height((*tree).right);
-
-return (left_height > right_height) ? (left_height + 1) : (right_height + 1);
 }
