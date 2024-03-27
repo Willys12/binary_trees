6495213978d147c5f09954996381326bea97a27c
Willys12 <ben.willys92@gmail.com>
2024-03-27 23:30:18 +0300

    Tree is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if tree is perfect.
+ * @tree: A pointer to a node of the BT.
+ * Return: 0 on success.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+if (tree == NULL)
+{
+return (0);
+}
+
+if (tree->left == NULL && tree->right == NULL)
+{
+return (1);
+}
+
+if (tree->left == NULL|| tree->right == NULL)
+{
+return (0);
+}
+
+if (binary_tree_is_perfect(tree->left) &&
+binary_tree_is_perfect(tree->right))
+{
+return (1);
+}
+return (0);
+}
