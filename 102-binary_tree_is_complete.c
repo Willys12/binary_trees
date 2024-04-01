78937010d13cd35fb2af2fded78f70134111a33a
Willys12 <ben.willys92@gmail.com>
2024-04-01 15:59:58 +0300

    Checking if BT is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,19 +1,30 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - Counts the nodes in BT.
- * @tree: a pointer to the root node of the tree.
- * Return: 1 if the tree is complete, 0 otherwise.
+ * binary_tree_height - measures the height of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ * Return: Height of the tree.
  */
-size_t binary_tree_nodes(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t left_height = 0;
+size_t right_height = 0;
 
-if (tree == NULL)
+if (tree)
+{
+if ((*tree).left)
+{
+left_height = 1 + binary_tree_height((*tree).left);
+}
+if ((*tree).right)
+{
+right_height = 1 + binary_tree_height((*tree).right);
+}
+return ((left_height > right_height) ? left_height : right_height);
+}
+else
 {
 return (0);
 }
-
-return (1 + binary_tree_nodes(tree->left) +
-binary_tree_nodes(tree->right));
 }
 
 /**
@@ -23,15 +34,15 @@ binary_tree_nodes(tree->right));
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-size_t countN_left, countN_right;
+int countN_left, countN_right;
 
 if (tree == NULL)
 {
 return (0);
 }
 
-countN_left = binary_tree_nodes(tree->left);
-countN_right = binary_tree_nodes(tree->right);
+countN_left = binary_tree_height(tree->left);
+countN_right = binary_tree_height(tree->right);
 
 if (countN_left == countN_right)
 {
